1dcd3c2d93b6dff8970a36531bb5ef415aa83354
Samuel Arrey Essim <arreyessimbate@hotmail.com>
2018-05-24 19:57:04 -0400

    Added more modules

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./config')
